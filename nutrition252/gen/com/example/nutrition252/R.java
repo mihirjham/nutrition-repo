0d66dd6dfd9f2221f6dd2d16aefdfc0f28115c82
mittalr <mittalr@purdue.edu>
2014-04-21 01:16:17 -0400

    Updated with UI

--- nutrition252/gen/com/example/nutrition252/R.java
+++ nutrition252/gen/com/example/nutrition252/R.java
@@ -8,6 +8,9 @@
 package com.example.nutrition252;
 
 public final class R {
+    public static final class array {
+        public static final int calories=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -23,17 +26,27 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int main_page=0x7f020001;
+        public static final int main_page_button=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int container=0x7f080000;
+        public static final int action_settings=0x7f09000a;
+        public static final int bGraph=0x7f090008;
+        public static final int bMealTable=0x7f090009;
+        public static final int bPostIt=0x7f090007;
+        public static final int etCalories=0x7f090003;
+        public static final int linearLayout1=0x7f090001;
+        public static final int linearLayout2=0x7f090004;
+        public static final int spinner=0x7f090006;
+        public static final int tvCalories=0x7f090002;
+        public static final int tvMeal=0x7f090005;
+        public static final int tvUserName=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
@@ -62,10 +75,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- nutrition252/src/com/example/nutrition252/MainActivity.java
+++ nutrition252/src/com/example/nutrition252/MainActivity.java
@@ -1,64 +1,73 @@
 package com.example.nutrition252;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
 
+public class MainActivity extends Activity implements OnItemSelectedListener {
+
+	Spinner spinner;
+	TextView username;
+	Button postIt;
+	Button graph;
+	Button mealTable;
+	EditText manualCalories;
+	Intent intent;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		
+		username = (TextView) findViewById(R.id.tvUserName);
+		postIt = (Button) findViewById(R.id.bPostIt);
+		graph = (Button) findViewById(R.id.bGraph);
+		mealTable = (Button) findViewById(R.id.bMealTable);
+		manualCalories = (EditText) findViewById(R.id.etCalories);
+		spinner = (Spinner) findViewById(R.id.spinner);
+		ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.calories, android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adapter);
+		spinner.setOnItemSelectedListener(this);;
+		
+		postIt.setOnClickListener((OnClickListener) this);
+		graph.setOnClickListener((OnClickListener) this);
+		mealTable.setOnClickListener((OnClickListener) this);
+		
 	}
-
+	public void onClick(View v) {
+		int id = v.getId();
+		if (id == R.id.bPostIt) {
+			/*intent = new Intent(v.getContext(), .class);
+			startActivityForResult(intent, 0);*/
+		} else if (id == R.id.bGraph) {
+			/*intent = new Intent(v.getContext(), .class);
+			startActivityForResult(intent, 0);*/
+		} else if (id == R.id.bMealTable) {
+			/*intent = new Intent(v.getContext(), .class);
+			startActivityForResult(intent, 0);*/
+		} 
+	}
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void onItemSelected(AdapterView<?> parent, View view, int position,
+			long id) {
+		// TODO Auto-generated method stub
+		TextView myText = (TextView) view;
+		Toast.makeText(this, "You Selected"+myText.getText(), Toast.LENGTH_SHORT).show();
 	}
-
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
+	public void onNothingSelected(AdapterView<?> parent) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
