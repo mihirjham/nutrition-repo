86e1fb44f0ef764d24f920a2ff3467416fc1eb6e
mittalr <mittalr@purdue.edu>
2014-04-28 02:33:58 -0400

    Fixed bugs with EditText -MainActivity.java

--- nutrition252/src/com/example/nutrition252/MainActivity.java
+++ nutrition252/src/com/example/nutrition252/MainActivity.java
@@ -18,8 +18,8 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
-public class MainActivity extends Activity implements AdapterView.OnItemSelectedListener, OnClickListener {
+public class MainActivity extends Activity implements
+		AdapterView.OnItemSelectedListener, OnClickListener {
 
 	private Spinner spinner;
 	private TextView username;
@@ -28,41 +28,46 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
 	private Button mealTable;
 	private EditText manualCalories;
 	private Intent intent;
-	String left,right,text;
+	String left, right, text;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		username = (TextView) findViewById(R.id.tvUserName);
 		postIt = (Button) findViewById(R.id.bPostIt);
 		graph = (Button) findViewById(R.id.bGraph);
 		mealTable = (Button) findViewById(R.id.bMealTable);
 		manualCalories = (EditText) findViewById(R.id.etCalories);
 		spinner = (Spinner) findViewById(R.id.spinner);
-		ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.calories, android.R.layout.simple_spinner_dropdown_item);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+				this, R.array.calories,
+				android.R.layout.simple_spinner_dropdown_item);
 		spinner.setAdapter(adapter);
-		spinner.setOnItemSelectedListener(this);;
+		spinner.setOnItemSelectedListener(this);
 		
-		postIt.setOnClickListener( this);
+
+		postIt.setOnClickListener(this);
 		graph.setOnClickListener(this);
 		mealTable.setOnClickListener(this);
-		
+
 	}
-	
+
 	public void onClick(View v) {
 		int id = v.getId();
 		if (id == R.id.bPostIt) {
-			//Send data entered to server
+			// Send data entered to server
 			Socket toServer;
-			try{
-				toServer = new Socket("sslab01.cs.purdue.edu",5555);
-				PrintWriter printwriter = new PrintWriter(toServer.getOutputStream(),true);
+			try {
+				toServer = new Socket("sslab01.cs.purdue.edu", 5555);
+				PrintWriter printwriter = new PrintWriter(
+						toServer.getOutputStream(), true);
 				sendToServer(printwriter);
 				toServer.close();
-			}
-			catch(IOException ioe){
-				Toast.makeText(this, "Couldn't Connect to Host", Toast.LENGTH_SHORT).show();
+			} catch (IOException ioe) {
+				Toast.makeText(this, "Couldn't Connect to Host",
+						Toast.LENGTH_SHORT).show();
 			}
 		} else if (id == R.id.bGraph) {
 			intent = new Intent(v.getContext(), Graph.class);
@@ -70,40 +75,51 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
 		} else if (id == R.id.bMealTable) {
 			intent = new Intent(v.getContext(), MealTable.class);
 			startActivityForResult(intent, 0);
-		} 
+		}
 	}
+
 	@Override
 	public void onItemSelected(AdapterView<?> parent, View view, int position,
 			long id) {
 		// TODO Auto-generated method stub
 		TextView myText = (TextView) view;
-		Toast.makeText(this, "You Selected "+myText.getText(), Toast.LENGTH_SHORT).show();
-		text = spinner.getSelectedItem().toString();
-		//Log.d("Selected",text);
-		int counter = text.indexOf('-');
-		if (counter >= 0) {
-		    left = text.substring(0, counter);
-		    right = text.substring(counter + 1);
-		    //Log.d("First",left);
-			//Log.d("Second",right);
-		} else {
-		  Log.d("Error", "There is no -");
+		Toast.makeText(this, "You Selected " + myText.getText(),
+				Toast.LENGTH_SHORT).show();
+		int indexPosition = spinner.getSelectedItemPosition();
+		Log.d("Position", "Selected position at: " + indexPosition);
+
+		if (position >= 1) {
+			text = spinner.getSelectedItem().toString();
+			// Log.d("Selected",text);
+			int counter = text.indexOf('-');
+			if (counter >= 0) {
+				left = text.substring(0, counter);
+				right = text.substring(counter + 1);
+				// Log.d("First",left);
+				// Log.d("Second",right);
+				manualCalories.setText(right, TextView.BufferType.EDITABLE);
+			} else {
+				Log.d("Error", "There is no -");
+			}
+		} else if (position == 0) {
+			manualCalories.setText(" ");
 		}
-		manualCalories.setText(right, TextView.BufferType.EDITABLE);
-		
+
 	}
+
 	@Override
 	public void onNothingSelected(AdapterView<?> parent) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	//sends info to server and closes printwriter
-	public void sendToServer(PrintWriter printwriter){
+
+	// sends info to server and closes printwriter
+	public void sendToServer(PrintWriter printwriter) {
 		String command = new String("STORE|");
 		command.concat(username.getText() + "|");
 		String selectedItem = spinner.getSelectedItem().toString();
-		selectedItem = selectedItem.replace('-','|');// separate the food item and calorie number
+		selectedItem = selectedItem.replace('-', '|');// separate the food item
+														// and calorie number
 		command.concat(selectedItem);
 		printwriter.print(command);
 		printwriter.close();
